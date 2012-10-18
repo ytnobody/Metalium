89897feb5045b452fc4b4e697ad2bd18d3191ad1
ytnobody <ytnobody@gmail.com>
2012-10-18 15:31:47 +0900

    initial commit

---
+++ lib/Metalium.pm
@@ -0,0 +1,32 @@
+package Metalium;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Metalium -
+
+=head1 SYNOPSIS
+
+  use Metalium;
+
+=head1 DESCRIPTION
+
+Metalium is
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
